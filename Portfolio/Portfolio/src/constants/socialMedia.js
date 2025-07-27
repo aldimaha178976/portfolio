5e247e20f850a7e11616b374655d0cee8cf8f53d
Aldi Maha <aldimaha36@gmail.com>
2025-07-27 14:55:30 +0700

    Menambahkan folder Portfolio

---
+++ Portfolio/Portfolio/src/constants/socialMedia.js
@@ -0,0 +1,35 @@
+import {
+    AiFillGithub,
+    AiFillInstagram,
+    AiFillLinkedin,
+    AiFillMail,
+    AiFillTwitterCircle
+} from "react-icons/all";
+
+export const SOCIAL_MEDIA = [
+    {
+        id: "social-media-1",
+        icon: AiFillLinkedin,
+        link: "https://www.linkedin.com/in/aldi-maha-148834334/",
+    },
+    {
+        id: "social-media-2",
+        icon: AiFillGithub,
+        link: "https://www.github.com/aldi_mh",
+    },
+    {
+        id: "social-media-3",
+        icon: AiFillMail,
+        link: "mailto:aldimaha36@gmai.com",
+    },
+    {
+        id: "social-media-4",
+        icon: AiFillInstagram,
+        link: "https://www.instagram.com/aldi_si_mh",
+    },
+    {
+        id: "social-media-5",
+        icon: AiFillTwitterCircle,
+        link: "https://twitter.com/Aldi_Maha",
+    },
+];
