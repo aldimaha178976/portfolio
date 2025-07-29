9cb26f65b8cf826dff486b8974dc80cf68d45fe9
Aldi Maha <aldimaha36@gmail.com>
2025-07-29 11:17:51 +0700

    Upload project portfolio asli

---
+++ src/constants/education.js
@@ -0,0 +1,14 @@
+
+import nitk from '../assets/images/Education/nitk.png';
+
+export const EDUCATION_LIST = [
+  {
+    id: "education-1",
+    icon: nitk,
+    title: "National Institute of Technology Karnataka, Surathkal",
+    degree: "Bachelor of Technology",
+    duration: "December 2021 - Juli 2025",
+    content1: "Major: Mechanical Engineering",
+    content2: "Minor: Information System",
+  },
+];
